8f9fac35fcde70f0b57b822d80ac833ca4462d4e
lukas braun <lukas.braun@mailbox.org>
2021-01-16 13:19:48 +0100

    Add Services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilesController } from './files/files.controller';
 import { TagsController } from './tags/tags.controller';
+import { FilesService } from './files/files.service';
+import { TagsService } from './tags/tags.service';
 
 @Module({
   imports: [],
   controllers: [AppController, FilesController, TagsController],
-  providers: [AppService],
+  providers: [AppService, FilesService, TagsService],
 })
 export class AppModule {}
