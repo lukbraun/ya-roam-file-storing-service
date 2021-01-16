e215eb2274ea49a79a2970c5cbf7572a0239bc6f
lukas braun <lukas.braun@mailbox.org>
2021-01-16 13:10:22 +0100

    Add controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilesController } from './files/files.controller';
+import { TagsController } from './tags/tags.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, FilesController, TagsController],
   providers: [AppService],
 })
 export class AppModule {}
